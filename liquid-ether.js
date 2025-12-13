9408f328fe29ae96fce633f6a5e6686b70bb39e9
Hars3n <harsen.schmidtk95@gmail.com>
2025-12-13 11:41:36 +0100

    Update liquid-ether.js

--- liquid-ether.js
+++ liquid-ether.js
@@ -3,8 +3,8 @@
     const mountRef = document.getElementById('liquidBg');
     if (!mountRef) return;
 
-    const mouseForce = 15;
-    const cursorSize = 80;
+    const mouseForce = 8;
+    const cursorSize = 60;
     const isViscous = false;
     const viscous = 20;
     const iterationsViscous = 25;
@@ -15,8 +15,8 @@
     const isBounce = false;
     const colors = ['#FF073A', '#FF0033', '#DC143C'];
     const autoDemo = true;
-    const autoSpeed = 0.3;
-    const autoIntensity = 1.5;
+    const autoSpeed = 0.20;
+    const autoIntensity = 0.9;
     const takeoverDuration = 0.25;
     const autoResumeDelay = 3000;
     const autoRampDuration = 0.6;
